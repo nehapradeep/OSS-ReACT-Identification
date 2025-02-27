c3e334f3323acd780a250e280de967b780426088
Pranav Wagh <pranav14112000@gmail.com>
2025-02-28 03:34:57 +0530

    code for PR related ReACTs
    (cherry picked from commit 5e1a58a7dffdd69c55a5d54779dfbff211afa304)

--- AnalyserScripts/identify_reacts_from_unstructured_data.py
+++ AnalyserScripts/identify_reacts_from_unstructured_data.py
@@ -1,9 +1,11 @@
 import os
 
 from google import genai
+#from google.generativeai import genai
 
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=GEMINI_API_KEY)
+#client = genai.configure(api_key=GEMINI_API_KEY)
 
 reacts = {
     "ReACT-17": "Encourage mentors to collaborate with mentees on addressing bugs or issues.",
@@ -29,4 +31,46 @@ with open("react_analysis_2.csv", "w") as f:
         )
         print(react, reacts[react], response.text)
         f.write(react + "," + reacts[react] + "," + response.text + "\n")
-        
+
+# Defining PR related ReACTS
+PR_reacts = {
+    "ReACT-5": "Utilize a pull-based development approach",
+    "ReACT-8": "Engage in code revision/Perform frequent code reviews.",
+    "ReACT-10": "Offer job support to the newcomer.",
+    "ReACT-22": "Encourage newcomers to share their work for increased exposure."
+}
+
+# Providing all the PR related files to the Gemini API
+pr_comments_file = client.files.upload(file="../github_api/kvrocks/pr_comments.txt")
+pr_file = client.files.upload(file="../github_api/kvrocks/pr.txt")
+
+
+# Reopening Output file
+with open("react_analysis_2.csv", "w") as f:
+    for react in PR_reacts:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=[
+                "Analyze the pull request comments and related PR descriptions of a GitHub project and tell me in one word either yes or no if the project follows this recommendation: " + PR_reacts[react],
+                pr_comments_file,
+                pr_file
+            ]
+        )
+
+        # Storing a quick explanation from Gemini too
+        explanation = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=[
+                "Provide a short, concise and to-the-point 3-4 lines max explanation on why do you think that the project does or does not follow this recommendation: " + PR_reacts[react] + " Directly start the output with explanation. Please give you answer in a paragraph style and not bullet points",
+                pr_comments_file,
+                pr_file
+            ]
+        )
+        print(react, PR_reacts[react], response.text, explanation.text)
+        f.write(react + "," + PR_reacts[react] + "," + response.text + "," + explanation.text + "\n")
+
+
+print("Hurray! done! PR ReACT analysis saved!")
+
+
+
